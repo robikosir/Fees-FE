00ecb78b45068df8ae4ce9c0b1029d72b72d11fc
Robert Košir <robikoser@gmail.com>
2021-07-30 09:11:48 +0200

    #4 move to the middle, add default Template

---
+++ layout/CenterLayout.vue
@@ -0,0 +1,44 @@
+<template>
+  <div class="h-100 wrapper">
+    <b-container
+      class="
+        h-100
+        d-flex
+        flex-column
+        text-lg-left text-center
+        pl-5
+        pr-5
+        pl-md-0
+        pr-md-0
+      "
+    >
+      <b-row
+        class="
+          justify-content-center
+          align-content-center
+          mt-lg-auto mt-md-5 mt-3
+        "
+      >
+        <b-col xl="9" lg="11" md="12">
+          <slot name="content" />
+        </b-col>
+      </b-row>
+      <b-row
+        class="mt-auto justify-content-center align-content-center"
+        style="height: 80px"
+      >
+        <b-col xl="9" lg="11" md="12">
+          <slot name="footer" />
+        </b-col>
+      </b-row>
+    </b-container>
+  </div>
+</template>
+
+<script>
+export default {
+  name: "CenterLayout",
+};
+</script>
+
+<style scoped></style>
--- src/views/Login.vue
+++ src/views/Login.vue
@@ -1,32 +1,39 @@
 <template>
-  <div>
-    <EmailInput
-      id="email"
-      :field="$v.form.email"
-      :state="validateState($v.form, 'email')"
-      @update="$v.form.email.$model = $event"
-      label="Email"
-      field-name="email"
-    />
-    <PasswordInput
-      id="password"
-      :field="$v.form.password"
-      :state="validateState($v.form, 'password')"
-      @update="$v.form.password.$model = $event"
-      label="password"
-      field-name="password"
-    />
-  </div>
+  <CenterLayout>
+    <template v-slot:content>
+      <b-row>
+        <b-col cols="12">
+          <EmailInput
+            id="email"
+            :field="$v.form.email"
+            :state="validateState($v.form, 'email')"
+            @update="$v.form.email.$model = $event"
+            label="Email"
+            field-name="email"
+          />
+          <PasswordInput
+            id="password"
+            :field="$v.form.password"
+            :state="validateState($v.form, 'password')"
+            @update="$v.form.password.$model = $event"
+            label="Password"
+            field-name="password"
+          />
+        </b-col>
+      </b-row>
+    </template>
+  </CenterLayout>
 </template>
 
 <script>
 import login from "@/mixins/auth/login";
 import EmailInput from "@/components/inputs/EmailInput";
 import PasswordInput from "@/components/inputs/PasswordInput";
+import CenterLayout from "../../layout/CenterLayout";
 
 export default {
   name: "Login",
-  components: { PasswordInput, EmailInput },
+  components: { CenterLayout, PasswordInput, EmailInput },
   mixins: [login],
   data() {
     return {
