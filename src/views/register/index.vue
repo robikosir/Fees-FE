cd4812277124293b35e961bdf6cfab7cda308cf5
Matěj <matej.pruner@gmail.com>
2021-08-02 23:26:06 +0200

    Registration form #8

--- src/views/register/index.vue
+++ src/views/register/index.vue
@@ -1,10 +1,76 @@
 <template>
-  <div>Register</div>
+  <CenterLayout>
+    <template v-slot:content>
+      <b-row>
+        <b-col cols="12">
+          <StringInput
+            id="firstName"
+            :field="$v.form.firstName"
+            :state="validateState($v.form, 'firstName')"
+            @update="$v.form.firstName.$model = $event"
+            label="First name"
+            field-name="firstName"
+          />
+          <StringInput
+            id="lastName"
+            :field="$v.form.lastName"
+            :state="validateState($v.form, 'lastName')"
+            @update="$v.form.lastName.$model = $event"
+            label="Last name"
+            field-name="lastName"
+          />
+          <EmailInput
+            id="email"
+            :field="$v.form.email"
+            :state="validateState($v.form, 'email')"
+            @update="$v.form.email.$model = $event"
+            label="Email"
+            field-name="email"
+          />
+          <PasswordInput
+            id="password"
+            :field="$v.form.password"
+            :state="validateState($v.form, 'password')"
+            @update="$v.form.password.$model = $event"
+            label="Password"
+            field-name="password"
+          />
+          <PasswordInput
+            id="repeatPassword"
+            :field="$v.form.repeatPassword"
+            :state="validateState($v.form, 'repeatPassword')"
+            @update="$v.form.repeatPassword.$model = $event"
+            label="Repeat paswword"
+            field-name="repeatPassword"
+          />
+          <b-button pill class="register" @click="next()">Register</b-button>
+        </b-col>
+      </b-row>
+    </template>
+  </CenterLayout>
 </template>
 
 <script>
+import registration from "@/mixins/auth/registration";
+import EmailInput from "@/components/inputs/EmailInput";
+import PasswordInput from "@/components/inputs/PasswordInput";
+import CenterLayout from "../../../layout/CenterLayout";
+import StringInput from "../../components/inputs/StringInput.vue";
+
 export default {
-  name: "Register",
+  name: "Registration",
+  components: { CenterLayout, PasswordInput, EmailInput, StringInput },
+  mixins: [registration],
+  data() {
+    return {
+      username: "",
+      password: "",
+      error: "",
+    };
+  },
+  methods: {
+    next() {},
+  },
 };
 </script>
 
