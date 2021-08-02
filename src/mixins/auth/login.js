d89cfa96b5fb7eba246a46cffe10d4eb7429c2e4
Matěj <matej.pruner@gmail.com>
2021-08-02 14:46:07 +0200

    Create registration mixin #7

--- src/mixins/auth/login.js
+++ src/mixins/auth/login.js
@@ -6,18 +6,31 @@ export default {
   data() {
     return {
       form: {
+        firstname: "",
+        lastname: "",
         email: "",
         password: "",
+        repeatpassword: "",
       },
     };
   },
   validations: {
     form: {
+      firstname: {
+        required,
+      },
+      lastname: {
+        required,
+      },
       email: {
         required,
         email,
       },
       password: {
+        required,
+        minLength: minLength(7),
+      },
+      repeatpassword: {
         required,
         minLength: minLength(7),
       },
@@ -28,5 +41,6 @@ export default {
       // TODO store auth token form the response to the store
       this.$router.push("/");
     },
+    register() {},
   },
 };
