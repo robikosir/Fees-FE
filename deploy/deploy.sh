e8d41d6e6f216d8cb616b89e464463c2fd469a58
Robert Košir <robikoser@gmail.com>
2023-03-06 14:59:19 +0100

    Fix deploy

--- deploy/deploy.sh
+++ deploy/deploy.sh
@@ -1,12 +1,12 @@
 #!/bin/bash
-sudo git checkout prod
+git checkout prod
 sudo echo '---------- Git checkout master complete ----------'
-sudo git fetch --all
+git fetch --all
 sudo echo '---------- Git fetch complete ----------'
-sudo git reset --hard origin/prod
+git reset --hard origin/prod
 sudo echo '---------- Git reset master complete ----------'
 sudo echo '---------- Docker compose build started ----------'
-sudo sudo docker-compose build
+sudo docker-compose build
 sudo echo '---------- Docker compose build complete ----------'
 sudo echo '---------- Docker compose deploy started ----------'
-sudo sudo docker-compose up -d
+sudo docker-compose up -d
